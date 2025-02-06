c3e5162ee214ee609fa8f05c24dba5d7c93a8261
parag <paragpatil187@gmail.com>
2025-02-06 19:06:38 +0530

    changes making common header in core file of backend for resolving cors error

--- backend/api/delete_user.php
+++ backend/api/delete_user.php
@@ -1,7 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
-header('Content-Type: application/json');
+
 require('../inc/core.php');
 $core = new Core();
 $request = new \stdClass();
--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -6,9 +6,9 @@ const List = () => {
   const [userData, setUserData] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
   const [modalType, setModalType] = useState("");
-    // const BASE_URL = "http://localhost/users_management/backend/api/";
-   const BASE_URL = "https://user--management.rf.gd/backend/api/";
-
+        const BASE_URL = "http://localhost/users_management/backend/api/";
+      //  const BASE_URL = "https://user--management.rf.gd/backend/api/";
+     
   // Fetch all users
   useEffect(() => {
     fetchUsers();
